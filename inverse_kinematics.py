05914e80b49eb0ff8a67402244ec9f628c315c36
Lucas Avila <lucas.soares.avila@gmail.com>
2019-11-10 23:29:26 -0300

    Found the ponit of the third joint

--- inverse_kinematics.py
+++ inverse_kinematics.py
@@ -14,11 +14,22 @@ R = Matrix([
 # Given the signal of the angle theta5
 # True = + and False = -
 
-signal = True
+sigq5 = True
+
+# Given the end-effector position
+
+px, py, pz = symbols('px, py, pz')
+
+PW = Matrix([
+    [px],
+    [py],
+    [pz],
+    [1]  # 0 or 1 ?
+])
 
 # Find q4, q5 and q6
 
-if signal:
+if sigq5:
     q4 = atan2(R[2], R[5])
     q5 = atan2(sqrt(R[2]**2 + R[5]**2), R[8])
     q6 = atan2(R[7], -R[6])
@@ -26,3 +37,11 @@ else:
     q4 = atan2(-R[2], -R[5])
     q5 = atan2(-sqrt(R[2]**2 + R[5]**2), R[8])
     q6 = atan2(-R[7], R[6])
+
+# Applie the forward kinematics to find the position of the third joint
+
+FK46 = FW4 * FW5 * FW6
+PA = FK46.inv() * PW
+
+# Find q1, q2 and q3
+
