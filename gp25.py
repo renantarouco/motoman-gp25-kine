d3bab9f021b36f9a4b4821e4bc6f327334b733f2
Renan Tarouco da Fonseca <renantarouco@gmail.com>
2019-11-14 00:32:02 -0300

    forward: Created DHForwardKine helper class

--- gp25.py
+++ gp25.py
@@ -5,7 +5,7 @@ from forward_kinematics import *
 init_printing()
 q1, q2, q3, q4, q5, q6 = symbols('theta1:7')
 
-FK = dh_t([
+fk = DHForwardKine([
   (q1, 505, 0, pi/2),
   (q2, 0, 150, 0),
   (q3, 0, 0, pi/2),
@@ -13,3 +13,8 @@ FK = dh_t([
   (q5, 0, 0, pi/2),
   (q6, 795, 0, 0)
 ])
+
+t = fk.tranformation_matrix()
+
+f1 = fk.link_transformation(1)
+f2 = fk.link_transformation(2)
